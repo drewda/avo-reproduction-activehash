11ed96e1da58896867216e049a3754af22576763
Adrian <adrian@adrianthedev.com>
2022-11-28 14:52:02 +0200

    allow gitpod host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Allow the gitpod url host
+  if ENV["GITPOD_WORKSPACE_URL"].present?
+    config.hosts << ENV["GITPOD_WORKSPACE_URL"]
+  end
 end
